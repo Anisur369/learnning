d196cfdcbce5d6fc2db495fd917529eca4f6e28b
Anisur369 <anisurrahman57136@gmail.com>
2025-07-09 22:43:31 +0600

    first commit

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Quote() {
     const [quote, setQuote] = useState('');
 
     useEffect(() => {
     async function fetchQuote() {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        const data = await response.json();
-        console.log(data);
-        setQuote(data);
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        console.log(response.data);
+        setQuote(response.data);
     }
     fetchQuote();
   }, []);
